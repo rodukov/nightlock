92dadf3584f244abe1ebf187348e0873681db7d2
138 <electroneuphoria@gmail.com>
2026-01-15 00:24:08 +0300

    expirements

--- include/init.h
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-#include <windows.h>
-#include <locale>
-
-void init();
--- include/utils.h
+++
@@ -1,6 +0,0 @@
-#include <string>
-
-std::string wstring_to_utf8(const std::wstring& wstr);
-std::wstring utf8_to_wstring(const std::string& str);
-std::string wstring_to_string(const std::wstring& wstr);
-std::wstring string_to_wstring(const std::string& str);
