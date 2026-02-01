30aface1d214214d1ded84b0c789ee8327c414c1
138 <electroneuphoria@gmail.com>
2026-02-01 16:39:46 +0300

    Qt

--- main.cpp
+++ main.cpp
@@ -1,49 +1,18 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-#include <QStandardItemModel>
-#include <QtQml>
-
-static QStandardItemModel *createDirModel()
-{
-    auto *model = new QStandardItemModel();
-    QStandardItem *root = model->invisibleRootItem();
-
-    auto *WORK = new QStandardItem("Work/");
-    auto *HIDDEN = new QStandardItem("Hidden/");
-    auto *BSUIR = new QStandardItem("BSUIR/");
-    auto *Yandex = new QStandardItem("Yandex/");
-
-    auto *Backend = new QStandardItem("Backend/");
-    auto *Frontend = new QStandardItem("Frontend/");
-    Yandex->appendRow(Frontend);
-    Yandex->appendRow(Backend);
-
-    WORK->appendRow(BSUIR);
-    WORK->appendRow(Yandex);
-
-    root->appendRow(WORK);
-    root->appendRow(HIDDEN);
-
-    return model;
-}
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
-    static QStandardItemModel *dirModel = createDirModel();
-
-    qmlRegisterSingletonInstance(
-        "Nightlock.Models", 1, 0,
-        "DirModel",
-        dirModel
-        );
-
     QQmlApplicationEngine engine;
-    engine.loadFromModule("nightlockQML", "Main");
-
-    if (engine.rootObjects().isEmpty())
-        return -1;
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreationFailed,
+        &app,
+        []() { QCoreApplication::exit(-1); },
+        Qt::QueuedConnection);
+    engine.loadFromModule("nightlock", "Main");
 
     return app.exec();
 }
